4c465ccab0ea8e80556768953946645866a7d771
chandrika chenna <chandrika.c@codemonk.io>
2025-06-30 13:26:41 +0530

    feat: portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
+      <a
+        href="https://www.instagram.com/embrace_code/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram size={24} />
+      </a>
+      <a
+        href="https://x.com/cHanDri_miniMe?t=CSZtl5lyhYCywXVp9po7tg&s=09"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaXTwitter size={24} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/chandrika-chenna/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={24} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
